348f4bfb4b8b638d34e2766deacde02f56277241
Joao marcos valente <joaomarcoslars.81@gmail.com>
2023-06-08 12:25:40 -0300

    testes com XUnit: Implementando os Moqs para o CreateProjectCommandHandler

---
+++ DevFreel.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Coree.Entities;
+using DevFreela.Coree.Repositories;
+using Moq;
+
+namespace DevFreel.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+                        //given dado q WHEN-qnd   THEN retorna                
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            //ARRANGE
+            var projectRepository = new Mock<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Titulo teste",
+                Description = "Descricao test",
+                TotalCost = 20000,
+                IdCliente = 2,
+                IdFreelancer = 3
+            };
+            //INIT COMMAND HANDLER
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepository.Object);
+
+            //ACT
+
+            /*retorna um ID de projeto*/
+            var id = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+            //ASSERTS
+            Assert.True(id >= 0);
+            /*Checar se o metodo AddAsync foi chamado*/
+            projectRepository.Verify(pr => pr.AddASync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
